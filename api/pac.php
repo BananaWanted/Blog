ccea3e7bdea80602ed4cc7d2030356948b65966a
BananaWanted <i@BananaWanted.com>
2016-09-24 03:56:55 +0800

    Add pac api

---
+++ api/pac.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+defined('_ZEXEC') or define("_ZEXEC", 1);
+require_once 'base.php';
+session_start();
+header("Content-Type:  application/json");
+
+$ret = array();
+
+foreach ()
