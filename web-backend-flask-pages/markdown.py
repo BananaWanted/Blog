b3df81af5af1c6445f64e94e5c4f90f4dc4c5fee
BananaWanted <i@BananaWanted.com>
2018-01-02 20:54:28 +0800

    add stub

--- web-backend-flask-pages/markdown.py
+++ web-backend-flask-pages/markdown.py
@@ -2,7 +2,17 @@
 import markdown
 
 m = markdown.Markdown(
-    extensions=['markdown.extensions.sane_lists'],
+    extensions=[
+        'markdown.extensions.sane_lists',
+        'markdown.extensions.extra',
+        'markdown.extensions.fenced_code',
+        'markdown.extensions.smart_strong',
+        'markdown.extensions.tables',
+        'markdown.extensions.def_list',
+        'markdown.extensions.headerid',
+        'markdown.extensions.nl2br',
+        'markdown.extensions.toc',
+    ],
     output_format='html5',
     lazy_ol=False
 )
